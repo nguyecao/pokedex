16544813637c99aa6c16136474d28786ebc133c6
nguyecao <nguyecao@oregonstate.edu>
2024-12-19 15:43:23 -0800

    added type checking

--- src/main/java/com/example/demo/pokemon/PokemonService.java
+++ src/main/java/com/example/demo/pokemon/PokemonService.java
@@ -47,11 +47,14 @@ public class PokemonService {
 
     @Transactional
     public void updatePokemon(Long pokemonId, String name, String[] type, Long evolutionId) {
+        String[] types = {"Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost", "Steel", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy"};
+
         Pokemon pokemon = pokemonRepository.findById(pokemonId)
                 .orElseThrow(() -> new IllegalStateException((
                         "pokemon with id " + pokemonId + " does not exist"
                         )));
-        if (name != null && name.length() > 0 && !Objects.equals(pokemon.getName(), name)) {
+
+        if (name != null && !name.isEmpty() && !Objects.equals(pokemon.getName(), name)) {
             pokemon.setName(name);
         }
 
@@ -59,7 +62,14 @@ public class PokemonService {
             pokemon.setEvolution(evolutionId);
         }
 
-        if (type != null && (pokemon.getType() == null || !Arrays.equals(type, pokemon.getType()))) {
+        boolean validType = true;
+        for (String t : type) {
+            if (!Arrays.stream(types).anyMatch(t::equals)) {
+                validType = false;
+            }
+        }
+
+        if (type != null && !Arrays.equals(type, pokemon.getType()) && validType && type.length <= 2) {
             pokemon.setType(type);
         }
     }
